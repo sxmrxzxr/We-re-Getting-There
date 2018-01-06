ade1988d96d1969b6130eb68089bc21ba520d5db
sxmrxzxr <tue95606@temple.edu>
2018-01-06 12:20:41 -0500

    individually create and destory passenger prefabs at stops

--- Assets/Scripts/StopController.cs
+++ Assets/Scripts/StopController.cs
@@ -17,6 +17,8 @@ public class StopController : MonoBehaviour {
     public GameObject passenger;
     public Transform passengerArea;
     public Transform disembarkArea;
+
+    private bool arePassengersDisembarked;
     
     private IEnumerator StartBoardingTimer()
     {
@@ -34,33 +36,37 @@ public class StopController : MonoBehaviour {
     {
         foreach (Passenger p in passengers)
         {
-            Vector3 pos = new Vector3(Random.Range(passengerArea.position.x, passengerArea.position.x + 0.25f), 0.25f, Random.Range(passengerArea.position.z, passengerArea.position.z + 0.5f));
+            Vector3 pos = new Vector3(Random.Range(passengerArea.position.x, passengerArea.position.x - 1.25f), 0.25f, Random.Range(passengerArea.position.z, passengerArea.position.z + 1.5f));
             GameObject newPassenger = Instantiate(passenger, pos, Quaternion.identity);
             newPassenger.transform.parent = passengerArea;
         }
     }
 
-    void RenderDisembarkingPassengers(List<Passenger> disembarkingPassengers)
+    private IEnumerator RenderDisembarkingPassengers(List<Passenger> disembarkingPassengers)
     {
         foreach (Passenger p in disembarkingPassengers.FindAll(d => d.Destination.stopName == this.stopName))
         {
-            new WaitForSeconds(1.0f);
-            Vector3 pos = new Vector3(disembarkArea.position.x, .25f, disembarkArea.position.z);
+            yield return new WaitForSeconds(1.0f);
+            Vector3 pos = new Vector3(Random.Range(disembarkArea.position.x, disembarkArea.position.x - 1.25f), .25f, Random.Range(disembarkArea.position.z, disembarkArea.position.z + 1.5f));
             GameObject newPassenger = Instantiate(passenger, pos, Quaternion.identity);
             newPassenger.transform.parent = disembarkArea;
         }
+
+        arePassengersDisembarked = true;
     }
 
-    void DestroyPassengers()
+    private IEnumerator DestroyPassengers()
     {
         foreach (Transform g in passengerArea.GetComponentsInChildren<Transform>())
         {
-            if (g.tag == "Passenger")
-            {
-                new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(1.0f);
+            if (g != null && g.tag == "Passenger")
+            {                
                 Destroy(g.gameObject);
             }
         }
+
+        visited = true;
     }
 
     void Start()
@@ -74,24 +80,22 @@ public class StopController : MonoBehaviour {
     void OnTriggerEnter(Collider c)
     {
         if (c.name == "Cube")
-            StartCoroutine(StartBoardingTimer());        
+        {
+            SimpleBusController bus = c.gameObject.GetComponentInParent<SimpleBusController>();
+            StartCoroutine(RenderDisembarkingPassengers(bus.currentPassengers));
+            bus.DisembarkPassengers(this);
+        }            
     }
 
     void OnTriggerStay(Collider c)
     {
         SimpleBusController bus = c.gameObject.GetComponentInParent<SimpleBusController>();
 
-        if (currentTimerValue == 0)
+        if (arePassengersDisembarked)
         {
-            visited = true;            
-            
             bus.BoardPassengers(passengers);
             passengers.RemoveRange(0, passengers.Count);
-
-            RenderDisembarkingPassengers(bus.currentPassengers);
-            bus.DisembarkPassengers(this);
-
-            DestroyPassengers();
+            StartCoroutine(DestroyPassengers());
         }
     }
 

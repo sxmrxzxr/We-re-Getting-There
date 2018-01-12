55ebf035849b47bc2f74736fe47b88352d4f1d3e
sxmrxzxr <tue95606@temple.edu>
2018-01-11 23:16:49 -0500

    Added functionality to buttons

---
+++ Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour {
+
+	public void QuitMe()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}

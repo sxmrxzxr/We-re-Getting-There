cda84afd4d623042935beeab71745b43585b4054
sxmrxzxr <tue95606@temple.edu>
2018-01-11 21:57:36 -0500

    added functionality to resume button on pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -57,19 +57,26 @@ public class GameManager : MonoBehaviour {
         StartCoroutine(StartTimer());
 	}
 
+    public void Pause()
+    {
+        if (!pauseCanvas.gameObject.activeInHierarchy)
+        {
+            pauseCanvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseCanvas.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
 	// Update is called once per frame
 	void Update () {
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (!pauseCanvas.gameObject.activeInHierarchy)
-            {
-                pauseCanvas.gameObject.SetActive(true);
-            }
-            else
-            {
-                pauseCanvas.gameObject.SetActive(false);
-            }
+            Pause();
         }
 
 		if (timerValue == 0 && !route.IsRouteComplete())
